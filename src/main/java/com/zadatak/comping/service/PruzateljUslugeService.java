e15a5d0630585a901bd43664d5c926593c546e6f
Marin <marinjure.jurlina11@gmail.com>
2024-02-21 14:02:44 +0100

    add exception handling

--- src/main/java/com/zadatak/comping/service/PruzateljUslugeService.java
+++ src/main/java/com/zadatak/comping/service/PruzateljUslugeService.java
@@ -11,17 +11,30 @@ public class PruzateljUslugeService {
     @Autowired
     private PruzateljUslugaRepository pruzateljUslugaRepository;
 
-    public void addNewPruzatelj(PruzateljUsluge pruzateljUsluge){
-        pruzateljUslugaRepository.save(pruzateljUsluge);
+    public void addNewPruzatelj(PruzateljUsluge pruzateljUsluge) {
+        try {
+            pruzateljUslugaRepository.save(pruzateljUsluge);
+        } catch (Exception e) {
+            System.out.println("Error while adding new PruzateljUsluge: " + e.getMessage());
+        }
     }
 
-    public void editPruzatelj(Long id, PruzateljUsluge pruzateljUsluge){
-        PruzateljUsluge pruzateljUslugeFetched = pruzateljUslugaRepository.getReferenceById(id);
-        pruzateljUslugeFetched.setNazivPruzatelja(pruzateljUsluge.getNazivPruzatelja());
-        pruzateljUslugaRepository.save(pruzateljUslugeFetched);
+    public void editPruzatelj(Long id, PruzateljUsluge pruzateljUsluge) {
+        try {
+            PruzateljUsluge pruzateljUslugeFetched = pruzateljUslugaRepository.getReferenceById(id);
+            pruzateljUslugeFetched.setNazivPruzatelja(pruzateljUsluge.getNazivPruzatelja());
+            pruzateljUslugaRepository.save(pruzateljUslugeFetched);
+        } catch (Exception e) {
+            System.out.println("Error while editing PruzateljUsluge: " + e.getMessage());
+        }
     }
 
-    public void deletePruzatelj(Long id){
-        pruzateljUslugaRepository.deleteById(id);
+    public void deletePruzatelj(Long id) {
+        try {
+            pruzateljUslugaRepository.deleteById(id);
+        } catch (Exception e) {
+            System.out.println("Error while deleting PruzateljUsluge: " + e.getMessage());
+        }
     }
 }
+
--- src/main/java/com/zadatak/comping/service/UslugaService.java
+++ src/main/java/com/zadatak/comping/service/UslugaService.java
@@ -20,38 +20,52 @@ public class UslugaService {
     @Autowired
     PruzateljUslugaRepository pruzateljUslugaRepository;
 
-    public void getOpisUsluga(){
-        List<UslugaOpisProjection> opisUsluga = uslugaRepository.findAllOpisUsluge();
+    public void getOpisUsluga() {
+        try {
+            List<UslugaOpisProjection> opisUsluga = uslugaRepository.findAllOpisUsluge();
 
-        for(UslugaOpisProjection projection: opisUsluga){
-            System.out.println(projection.getOpisUsluge());
+            for (UslugaOpisProjection projection : opisUsluga) {
+                System.out.println(projection.getOpisUsluge());
+            }
+        } catch (Exception e) {
+            System.out.println("Error while fetching opisUsluga: " + e.getMessage());
         }
     }
 
     public void addNewUsluga(Usluga usluga) {
-        Usluga newUsluga = new Usluga();
-        newUsluga.setOpisUsluge(usluga.getOpisUsluge());
+        try {
+            Usluga newUsluga = new Usluga();
+            newUsluga.setOpisUsluge(usluga.getOpisUsluge());
 
-        List<Long> pruzateljUslugeIds = usluga.getPruzateljiUsluge().stream()
-                .map(PruzateljUsluge::getId)
-                .collect(Collectors.toList());
+            List<Long> pruzateljUslugeIds = usluga.getPruzateljiUsluge().stream()
+                    .map(PruzateljUsluge::getId)
+                    .collect(Collectors.toList());
 
-        List<PruzateljUsluge> pruzateljiUslugeEntities = pruzateljUslugaRepository.findAllById(pruzateljUslugeIds);
+            List<PruzateljUsluge> pruzateljiUslugeEntities = pruzateljUslugaRepository.findAllById(pruzateljUslugeIds);
 
-        newUsluga.getPruzateljiUsluge().addAll(pruzateljiUslugeEntities);
+            newUsluga.getPruzateljiUsluge().addAll(pruzateljiUslugeEntities);
 
-        uslugaRepository.save(newUsluga);
+            uslugaRepository.save(newUsluga);
+        } catch (Exception e) {
+            System.out.println("Error while adding new usluga: " + e.getMessage());
+        }
     }
 
-
-
-    public void editUsluga(Long id, Usluga usluga){
-        Usluga uslugaFetched = uslugaRepository.getReferenceById(id);
-        uslugaFetched.setOpisUsluge(usluga.getOpisUsluge());
-        uslugaRepository.save(uslugaFetched);
+    public void editUsluga(Long id, Usluga usluga) {
+        try {
+            Usluga uslugaFetched = uslugaRepository.getReferenceById(id);
+            uslugaFetched.setOpisUsluge(usluga.getOpisUsluge());
+            uslugaRepository.save(uslugaFetched);
+        } catch (Exception e) {
+            System.out.println("Error while editing usluga: " + e.getMessage());
+        }
     }
 
     public void deleteUsluga(Long id) {
-        uslugaRepository.deleteById(id);
+        try {
+            uslugaRepository.deleteById(id);
+        } catch (Exception e) {
+            System.out.println("Error while deleting usluga: " + e.getMessage());
+        }
     }
 }
